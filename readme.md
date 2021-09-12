6c3788c287e0bcfa3155e5274a91bb13fb8c8cc9
André <andre.niklasson19@gmail.com>
2021-09-12 14:42:08 +0200

    Now detaches instance from autoscaling group. Reads node name from Env var. Drains the node in background process

--- readme.md
+++ readme.md
@@ -1,11 +1,8 @@
 # Python Spot Interrupt Handler
-Given a spot termination notice, the spot-interrupt-handler deregisters the instance from associated ELB Target Groups and drains the node for graceful pod termination.
+Given a spot termination notice, the spot-interrupt-handler detaches the instance from its associated Autoscaling group and drains the node for graceful pod termination.
 
 ### Node Selection
 Label your nodes to avoid having the handler running on your On Demand instances. Label your Spot Instances with ```lifecycle=Ec2Spot``` and uncomment the nodeSelector block in the DaemonSet specified in _./interrupt-handler.yaml_.
-
-### Target Groups Tags
-The interrupt-handler identifies the associated Target Groups to be deregistred from with a given tag, namely ```spot-interrupt-handler/enabled : true```. 
 
 ### Permissions
 The interrupt-handler requires following IAM permissions to deregister the instance from the Target Group:
@@ -16,7 +13,7 @@ The interrupt-handler requires following IAM permissions to deregister the insta
 - elasticloadbalancing:DescribeTargetGroups
 - tag:GetResources
 
-Recommended is to use resources like kube2iam, kiam, or newly released IAM Roles for ServiceAccounts:
+Recommended is to use resources like kube2iam, kiam, or IAM Roles for ServiceAccounts:
 https://docs.aws.amazon.com/eks/latest/userguide/iam-roles-for-service-accounts.html 
 
 ### Build and deploy
@@ -24,4 +21,4 @@ Build the image using the Dockerfile present in the repo. Push the image to your
 
 ### Recommended resources
 - https://ec2spotworkshops.com/
-- https://github.com/mumoshu/kube-spot-termination-notice-handler Does not deregister the instance from any load balancer, but the project inspired me
+- https://github.com/mumoshu/kube-spot-termination-notice-handler
--- spot-interrupt-handler.py
+++ spot-interrupt-handler.py
@@ -4,65 +4,72 @@ import subprocess
 import time
 import boto3
 import json
+import sys
 
 INSTANCE_ID_URL="http://169.254.169.254/latest/meta-data/instance-id"
 TERMINATION_URL="http://169.254.169.254/latest/meta-data/spot/termination-time"
-DOCUMENT_URL="http://169.254.169.254/latest/dynamic/instance-identity/document"
-
 POLL_INTERVAL=5
 
-instanceId = requests.get(INSTANCE_ID_URL).text
-print("InstanceID: " + instanceId)
+asg_client = boto3.client('autoscaling')
 
-document = requests.get(DOCUMENT_URL).text
-region = json.loads(document)['region']
-print("Region: " + region)
+def wait_for_termination_notice():
+    print("Polling " + TERMINATION_URL + " every " + str(POLL_INTERVAL) + " second(s)")
+    while requests.get(TERMINATION_URL).status_code != 200:
+        time.sleep( POLL_INTERVAL )
+        print("No interruption notice")
+    print("Interruption notice received!")
 
-resourceClient = boto3.client('resourcegroupstaggingapi', region_name=region)
-elbClient = boto3.client('elbv2', region_name=region)
+def drain_node(node_name):
+    drain_cmd="kubectl drain " + node_name + " --force --ignore-daemonsets --delete-local-data --grace-period=120"
+    print("Draining the node...")
+    subprocess.Popen(drain_cmd, shell=True)
 
-print("Polling " + TERMINATION_URL + " every " + str(POLL_INTERVAL) + " second(s)")
-while requests.get(TERMINATION_URL).status_code is not 200:
-    time.sleep( POLL_INTERVAL )
-    print("Status: alive")
-
-## Deregistering from ELB
-try:
-    resourceResponse = resourceClient.get_resources(
-        TagFilters=[
-            {
-                'Key': 'spot-interrupt-handler/enabled',
-                'Values': [
-                    'true',
-                ]
-            },
-        ],
-        ResourcesPerPage=100,
-        ResourceTypeFilters=[
-            'elasticloadbalancing:targetgroup',
-        ]
-    )
-    for targetGroupArn in resourceResponse['ResourceTagMappingList']:
-        print(targetGroupArn['ResourceARN'])
-        elbResponse = elbClient.deregister_targets(
-            TargetGroupArn = targetGroupArn['ResourceARN'],
-            Targets=[
-                {
-                    'Id': instanceId,
-                },
-            ]
+def fetch_autoscaling_group(instance_id):
+    response = asg_client.describe_auto_scaling_groups()
+    result_list = response["AutoScalingGroups"]
+    while "NextToken" in response:
+        response = asg_client.describe_auto_scaling_groups(
+            NextToken=response["NextToken"]
         )
+        result_list = result_list + response["AutoScalingGroups"]
+
+    for autoscaling_group in result_list:
+        for instance in autoscaling_group["Instances"]:
+            if instance_id in instance["InstanceId"]:
+                return autoscaling_group
+
+def detach_instance_from_asg(autoscaling_group, instance_id):
+    asg_name = autoscaling_group["AutoScalingGroupName"]
+    current_capacity = autoscaling_group["DesiredCapacity"]
+    asg_client.detach_instances(
+        InstanceIds=[
+            instance_id,
+        ],
+        AutoScalingGroupName=asg_name,
+        ShouldDecrementDesiredCapacity=False
+    )
+
+try:
+    instance_id = requests.get(INSTANCE_ID_URL).text
+    autoscaling_group = fetch_autoscaling_group(instance_id)
 except:
-    print("Error deregistering from ELB")
+    print("Unable to fetch autoscaling group, Exiting.....")
+    sys.exit(1)
 
-## Drain the node
-nodeCmd="kubectl --namespace " + os.environ['NAMESPACE'] + " get pod "+ os.environ['POD_NAME'] +" --output jsonpath=\"{.spec.nodeName}\""
-nodeName = subprocess.check_output(nodeCmd, shell=True).decode('ascii')
+if "NODE_NAME" in os.environ:
+    node_name = os.environ['NODE_NAME']
+else:
+    print("NODE_NAME ENV variable is not set! Exiting.....")
+    sys.exit(1)
 
-drainCmd="kubectl drain " + nodeName + " --force --ignore-daemonsets --delete-local-data"
-print(drainCmd)
-res = subprocess.check_output(drainCmd, shell=True).decode('ascii')
-print(res)
+wait_for_termination_notice()
+ print("InstanceID: " + instance_id + " has reveived spot termination notice")
 
+drain_node(node_name) # Runs as a parallel process
+try:
+    detach_instance_from_asg(autoscaling_group, instance_id)
+except Exception as e:
+    print("Error trying to detach the instance from autoscaling group")
+    print(e)
 time.sleep( 500 )
 
